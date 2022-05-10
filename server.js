64236259662a06349b93c2494fa9a39cd367ef31
Ange Noutchogbe <angenoutchogbe@gmail.com>
2022-05-11 00:46:49 +0100

    mail function

--- server.js
+++ server.js
@@ -2,14 +2,58 @@ var express = require('express');
 var bodyParser=require("body-parser");
 var countries=require("countries-list");
 // upload dependances
+// Env
+require('dotenv').config();
+const MongoClient = require('mongoose');
 const fs=require('fs')
 var multer=require("multer")
 const path=require("path")
 
+// email pulgin
+var nodemailer = require('nodemailer');
+
+//mail functions
+var transporter = nodemailer.createTransport({
+  service: process.env.SERVICE_MAIL, // à definir dans .env
+  auth: {
+    user: process.env.EMAIL_ADDRESS,// à definir dans .env
+    pass: process.env.EMAIL_PASSWORD// à definir dans .env
+  }
+});
+
+// for only one person
+function getMailOptions(recipients,message,subject) {
+  var isHtml=message[0]=="<"
+  var options={
+    from:  process.env.EMAIL_ADDRESS,
+    to: recipients,
+    subject: subject,
+    text: message,
+    }
+  isHtml? options['html']=message:options['text']=message
+}
+
+
+// sendMail function 
+function sendMails() {
+  var mailOptions=getMailOptions("angenoutchogbe@gmail.com", //ecrivez votre mail ici pour recevoir le mail
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+  "BRANCH");
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+
+
+// engine
 var app = express();
-// Env
-require('dotenv').config();
-const MongoClient = require('mongoose');
+
+
 
 let imageShema = {
      name:String,
@@ -61,6 +105,7 @@ app.use("/src/js",express.static(__dirname+"/src/js"));
 
 // '/' est la route racine
 app.get('/', function (req, res) {
+
   res.render(__dirname+"/src/views/welcome.ejs");
 });
 
